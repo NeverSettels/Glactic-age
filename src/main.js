436e6630c2811e6ff1d00be8ab59b94d0f1a0866
NeverSettels <settels.eric@gmail.com>
2020-04-17 13:15:46 -0700

    add css and main.js code

---
+++ src/main.js
@@ -0,0 +1,9 @@
+//import { Planet , User } from './age.logic.js';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+$(document).ready(() => {
+
+}); 
