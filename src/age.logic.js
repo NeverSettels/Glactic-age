1dfe23a55f55d29e7bc257cc83b03e09ad2edb17
NeverSettels <settels.eric@gmail.com>
2020-04-17 09:35:43 -0700

    getting wierd jest message

--- src/age.logic.js
+++ src/age.logic.js
@@ -9,6 +9,16 @@ export class Planet {
   calculateExpectancy(years, expectancy) {
     let planetYears = this.calculateYears(years);
     let planetExpectancy = this.calculateYears(expectancy)
-    return planetYears;
+    let overUnderEqual = ""
+    let calculatedExpectancy = planetExpectancy - planetYears;
+    if (calculatedExpectancy > 0) {
+      overUnderEqual = 'under'
+    } else if (calculatedExpectancy < 0) {
+      overUnderEqual = "over";
+      calculatedExpectancy *= -1
+    } else {
+      overUnderEqual = "equal";
+    }
+    return [calculatedExpectancy, overUnderEqual];
   }
 }
